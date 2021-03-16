aea72aa031a0685f9e6fe993ebe8f90a7b37550b
Sim <sketema@uncc.edu>
2021-03-15 20:39:41 -0400

    lab 6 updated

--- lab6/rest-api-swagger/internal/main.go
+++ lab6/rest-api-swagger/internal/main.go
@@ -1,51 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"html"
 	"log"
-
-	"github.com/go-openapi/loads"
-	"github.com/go-openapi/runtime/middleware"
-	"github.com/go-swagger/go-swagger/examples/oauth2/restapi"
+	"net/http"
 )
 
 func main() {
-
-	// Initialize Swagger
-	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	api := operations.NewHelloAPIAPI(swaggerSpec)
-	server := restapi.NewServer(api)
-
-	defer func() {
-		if err := server.Shutdown(); err != nil {
-			// error handle
-			log.Fatalln(err)
-		}
-	}()
-
-	server.Port = 8080
-
-	// Implement the CheckHealth handler
-	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
-
-	// Implement the GetHelloUser handler
-	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
-
-	// Start server which listening
-	if err := server.Serve(); err != nil {
-		log.Fatalln(err)
-	}
-}
-
-//Health route returns OK
-func Health(operations.CheckHealthParams) middleware.Responder {
-	return operations.NewCheckHealthOK().WithPayload("OK\n")
-}
-
-//GetHelloUser returns Hello + your name
-func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
-	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+	})
+	log.Println("Listening on localhost: 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
