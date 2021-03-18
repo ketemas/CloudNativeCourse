7eddc5369e53808d008d1095a3adae579f3c57b1
Sim <sketema@uncc.edu>
2021-03-17 22:36:38 -0400

    lab5

--- lab5/movieclient/client.go
+++ lab5/movieclient/client.go
@@ -6,20 +6,14 @@ import (
 	"log"
 	"os"
 	"time"
-	"strings"
-	"strconv"
 
-	"movieapi"
+	"github.com/ketemas/CloudNativeCourse/tree/main/lab5/movieapi"
 	"google.golang.org/grpc"
 )
 
 const (
 	address      = "localhost:50051"
 	defaultTitle = "Pulp fiction"
-	inTitle 	 = "LOTR"
-	inYear 		 = "2001"
-	inDirector   = "Peter Jackson"
-	inCast 		 = "Viggo Mortensen, Elijah wood, Ian Mckellan, Sean Astin"
 )
 
 func main() {
@@ -33,27 +27,12 @@ func main() {
 
 	// Contact the server and print out its response.
 	title := defaultTitle
-	year := inYear
-	director := inDirector
-	cast := inCast
 	if len(os.Args) > 1 {
 		title = os.Args[1]
-		year = os.Args[2]
-		director = os.args[3]
-		cast = os.Args[4]
 	}
 	// Timeout if server doesn't respond
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	yearVala, err := strconv.Atol(year)
-
-	//Set Movie
-	s, err := c.SetMovieInfo(ctx, &movieapi.MovieData(Title: title, Year: int32(yearVal), Director: director, Cast: strings.Split(cast, ",")))
-	if err := nil {
-		log.Fatalf("cloud not get Movie info: %v", err)
-	}
-	log.printf("status: %s", s)
-
 	r, err := c.GetMovieInfo(ctx, &movieapi.MovieRequest{Title: title})
 	if err != nil {
 		log.Fatalf("could not get movie info: %v", err)
--- lab5/movieserver/server.go
+++ lab5/movieserver/server.go
@@ -8,8 +8,7 @@ import (
 	"strconv"
 	"strings"
 
-	"movieapi"
-
+	"github.com/ketemas/CloudNativeCourse/tree/main/lab5/movieapi"
 	"google.golang.org/grpc"
 )
 
@@ -45,27 +44,6 @@ func (s *server) GetMovieInfo(ctx context.Context, in *movieapi.MovieRequest) (*
 	}
 
 	return reply, nil
-
-}
-
-func (s *server) SetMovieInfo(ctx context.Context, in *Movieapi.MovieData) (*movieapi.Status, error) {
-	title := in.GetTitle()
-	year := in.GetYear()
-	director := in.GetDirector()
-	cast := strings.Join(in.GetCast(), ",")
-	log.printf("Received: %v, %s, %d, %f", title, year, director, cast)
-	status := &movieapi.Status{}
-	if _, ok := moviedb[title]; ok { // Title present in database
-		status.Code = "Movie already present."
-		return status, nil
-	} else {
-		yearString := strconv.Itoa(int(year))
-		data := []string{yearString, director, cast}
-		moviedb[title] = data
-		status.Code = "Movie added to Database!"
-	}
-
-	return status, nil
 
 }
 
